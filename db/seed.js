00bf73024d67065b7dff0ef880afc48eb1487bae
Scott Harlan <seharlan@gmail.com>
2020-03-26 16:04:34 -0700

    film seed

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,11 @@
 const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
 const Reviewer = require('../lib/models/Reviewer');
+const Film = require('../lib/models/Film');
 const chance = require('chance').Chance();
 
 module.exports = async() => {
-  const studios = await Studio.create([...Array(10)].map(() => ({
+  const studios = await Studio.create([...Array(3)].map(() => ({
     name: chance.company(),
     address: {
       city: chance.city(),
@@ -13,14 +14,26 @@ module.exports = async() => {
     }
   })));
 
-  const actors = await Actor.create([...Array(30)].map(() => ({
+  const actors = await Actor.create([...Array(10)].map(() => ({
     name: chance.name(),
     dob: chance.date(),
     pob: chance.city()
   })));
 
-  const reviewer = await Reviewer.create([...Array(50)].map(() => ({
+  const reviewers = await Reviewer.create([...Array(30)].map(() => ({
     name: chance.name(),
     company: chance.company()
+  })));
+
+  const films = await Film.create([...Array(5)].map(() => ({
+    title: `Your ${chance.profession()} is a ${chance.animal()}?!`,
+    studio: chance.pickone(studios)._id,
+    released: chance.year(),
+    cast: [
+      { role: chance.animal(), actor: chance.pickone(actors)._id },
+      { role: chance.animal(), actor: chance.pickone(actors)._id },
+      { role: chance.animal(), actor: chance.pickone(actors)._id },
+      { role: chance.animal(), actor: chance.pickone(actors)._id }
+    ]
   })));
 };

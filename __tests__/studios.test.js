6956210ca4ed54ae4bc1ffc9cf02b7bb3273c7ed
Scott Harlan <seharlan@gmail.com>
2020-03-26 14:48:53 -0700

    studio test made and failing

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -6,7 +6,31 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
 describe('app routes', () => {
-  it('dumb', () => {
-    expect(true).toEqual(true);
+  it('creates a Studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'test studio',
+        address: {
+          city: 'test city',
+          state: 'test state',
+          country: 'test country'
+        }
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'test studio',
+          address: {
+            city: 'test city',
+            state: 'test state',
+            country: 'test country'
+          },
+          _id: expect.any(String),
+          __V: 0
+        });
+      });
   });
+ 
+  //get by id
+  //get all
 });

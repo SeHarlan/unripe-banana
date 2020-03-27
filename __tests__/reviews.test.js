43f076ea5ed720dbc4171bbf22ed89f8dd264f59
Scott Harlan <seharlan@gmail.com>
2020-03-26 17:57:27 -0700

    review post test passes

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -1,11 +1,32 @@
-const { getFilm, getFilms, getActor, getStudio } = require('../db/data-helpers');
+const { getReviews, getFilm, getReviewer, getReview } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('review tests', () => {
-  it('dumby', () => {
-    expect(true).toEqual(true);
+  it('creates a new review', async() => {
+    const reviewer = await getReviewer();
+    const film = await getFilm();
+    return request(app)
+      .post('/api/v1/reviews')
+      .send({
+        rating: 3,
+        reviewer: reviewer._id,
+        review: 'test review',
+        film: film._id
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          rating: 3,
+          reviewer: expect.any(String),
+          review: 'test review',
+          film: expect.any(String),
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
   });
+  //get top 100
+  //delete
 })
 ;
